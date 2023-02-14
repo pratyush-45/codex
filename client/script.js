c132229da65808520a07f750fa50ed772cac2aac
pratyush-45 <pratyush4599@gmail.com>
2023-02-14 23:47:58 +0530

    Updated Server Endpoint

--- client/script.js
+++ client/script.js
@@ -86,7 +86,8 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server -> bot's response
-  const response = await fetch("http://localhost:5000", {
+  // http://localhost:5000
+  const response = await fetch("https://codex-server-lxut.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
